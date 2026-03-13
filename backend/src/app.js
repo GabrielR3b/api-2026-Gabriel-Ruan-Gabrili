e1dcfc696e06335691099f36117efb2ef32f0793
GabrielR3b <ruanfelixxx@github.com>
2026-03-13 09:59:20 -0300

    Adiçaão para o render

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,18 @@
 import express from "express";
 const app = express();
-app.listen(3000, () => {
-console.log("Server running on localhost:3000");
-});
 
 app.get("/", (req, res) => {
-res.send({message: "Servidor em Execução", success: true });
+    res.send({message: "Servidor em Execução", success: true });
 });
 
 app.get("/equipe", (req, res) => {
-res.send([
-{nome: "Fulano", curso: "TI"},
-{nome: "Fulano", curso: "TI"},
-{nome: "Ciclano", curso: "TI"},
+    res.send([
+        {nome: "Fulano", curso: "TI"},
+        {nome: "Fulano", curso: "TI"},
+        {nome: "Ciclano", curso: "TI"},
 ]);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server running on localhost:3000");
 });
