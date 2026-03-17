f51fa4c5bc31985d2af02e5db3af5b8c53cb0762
Ruan <ruan.felix10@aluno.ifce.edu.br>
2026-03-17 16:04:27 -0300

    Rota do Ruan Criada

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,25 +2,33 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send({message: "Servidor em Execução", success: true });
+  res.send({ message: "Servidor em Execução", success: true });
 });
 
 app.get("/equipe", (req, res) => {
-    res.send([
-        {nome: "Fulano", curso: "TI"},
-        {nome: "Fulano", curso: "TI"},
-        {nome: "Ciclano", curso: "TI"},
-]);
+  res.send([
+    { nome: "Fulano", curso: "TI" },
+    { nome: "Fulano", curso: "TI" },
+    { nome: "Ciclano", curso: "TI" },
+  ]);
 });
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Server running on localhost:3000");
+  console.log("Server running on localhost:3000");
 });
 
-app.get('/gabriel-ruan-gabrili/gabriel', (req, res) => { 
-    res.json({ 
-        nome: "Gabriel", 
-        matricula: "20251131000248", 
-        info: "Apoiador do Intercurso" 
-    }); 
+app.get("/gabriel-ruan-gabrili/gabriel", (req, res) => {
+  res.json({
+    nome: "Gabriel",
+    matricula: "20251131000248",
+    info: "Apoiador do Intercurso",
+  });
+});
+
+app.get("/gabriel-ruan-gabrili/ruan", (req, res) => {
+  res.json({
+    nome: "Ruan",
+    matricula: "20251131000094",
+    info: "Aluno de Info II",
+  });
 });
